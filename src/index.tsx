c12f52c30ce50922fecd93d918138124ecc8cb69
Pau <103753396+pau-develop@users.noreply.github.com>
2022-08-29 22:57:19 +0200

    Installed styled comps, created color variabled and theme wrap

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import { store } from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
+import { ThemeProvider } from "styled-components";
+import styledTheme from "./styledTheme";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -12,7 +14,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={styledTheme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
---
+++ src/styledTheme.ts
@@ -0,0 +1,7 @@
+const styledTheme = {
+  primaryColor: "#144573",
+  secondaryColor: "#A1CEF6",
+  thirdColor: "#17232E",
+};
+
+export default styledTheme;
