dd3ca167561707ba851c8aa9420dcf971b098c14
Pau <103753396+pau-develop@users.noreply.github.com>
2022-08-30 22:00:29 +0200

    Tested if statement in useUsers hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "./app/store";
 import AppStyled from "./AppStyled";
 import Header from "./components/Header/Header";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
 
 const App = (): JSX.Element => {
-  const ui = useSelector<RootState>((state) => state.ui);
-
   return (
     <AppStyled className="app-container">
       <Header />
