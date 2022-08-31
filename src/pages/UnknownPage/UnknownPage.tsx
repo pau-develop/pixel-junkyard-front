06da708e48c4a356b0e837a7228e01955100b16d
Pau <103753396+pau-develop@users.noreply.github.com>
2022-08-31 10:37:32 +0200

    Added redirect functionality to UnknownPage

--- src/pages/UnknownPage/UnknownPage.tsx
+++ src/pages/UnknownPage/UnknownPage.tsx
@@ -1,20 +1,28 @@
+import { useNavigate } from "react-router-dom";
 import UnknownPageStyled from "./UnknownPageStyled";
 
 const UnknownPage = (): JSX.Element => {
+  const navigate = useNavigate();
+
   let timer = 3;
-  const redirectCounter = () => {
+  const counter = () => {
     const interval = setInterval(() => {
       timer -= 1;
-      console.log(timer);
+      if (timer <= 0) {
+        clearInterval(interval);
+        navigate(-1);
+      }
     }, 1000);
   };
+
+  counter();
 
   return (
     <UnknownPageStyled className="unknown-path">
       <div className="unknown-path__box">
         <h2>ERROR 404</h2>
         <h3>Page not found</h3>
-        <p>Redirecting to homepage...</p>
+        <p>Redirecting to previous page...</p>
       </div>
     </UnknownPageStyled>
   );
