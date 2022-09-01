589806c017db041754f96b2612440d64dea3fa12
Pau <103753396+pau-develop@users.noreply.github.com>
2022-09-01 09:26:25 +0200

    Created LoginFormPage

---
+++ src/pages/LoginFormPage/LoginFormPage.tsx
@@ -0,0 +1,14 @@
+import LoginForm from "../../components/LoginForm/LoginForm";
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
+import PageStyled from "../PageStyled";
+
+const LoginFormPage = (): JSX.Element => {
+  return (
+    <PageStyled className="page">
+      <h1 className="page__title">Log in</h1>
+      <LoginForm />
+    </PageStyled>
+  );
+};
+
+export default LoginFormPage;
