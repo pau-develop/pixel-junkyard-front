b132c527c1e2de2aefc2cb9703f23733e3b75398
Pau <103753396+pau-develop@users.noreply.github.com>
2022-09-02 23:52:52 +0200

    Implemented logout functionality in LogoutMenu component

--- src/components/LogoutMenu/LogoutMenu.test.tsx
+++ src/components/LogoutMenu/LogoutMenu.test.tsx
@@ -1,10 +1,31 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "../../app/store";
 import LogoutMenu from "./LogoutMenu";
+
+interface WrapperProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+let Wrapper: ({ children }: WrapperProps) => JSX.Element;
+
+beforeEach(() => {
+  Wrapper = ({ children }: WrapperProps): JSX.Element => {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </Provider>
+    );
+  };
+});
 
 describe("Given a LogoutMenu component", () => {
   describe("When instantiated", () => {
     test("It should the text 'Do you want to log out?'", () => {
-      render(<LogoutMenu menuClass="phone-logout-menu" />);
+      render(<LogoutMenu menuClass="phone-logout-menu" action={() => null} />, {
+        wrapper: Wrapper,
+      });
       const spanElement = screen.getByText("Do you want to log out?");
 
       expect(spanElement).not.toBeNull();
