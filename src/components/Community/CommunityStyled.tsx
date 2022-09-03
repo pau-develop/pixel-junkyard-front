2a9bdf31ec5bb31063c909ea101d85ddfdb6f65b
Pau <103753396+pau-develop@users.noreply.github.com>
2022-09-03 15:50:18 +0200

    Styled Community component

--- src/components/Community/CommunityStyled.tsx
+++ src/components/Community/CommunityStyled.tsx
@@ -3,13 +3,68 @@ import styled from "styled-components";
 const CommunityStyled = styled.div`
   width: 100%;
   padding: 0 5%;
+  overflow: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
   ul {
     list-style: none;
     padding: 0;
     color: ${(props) => props.theme.secondaryColor};
+    display: flex;
+    flex-direction: column;
     li {
       background-color: ${(props) => props.theme.primaryColor};
       border: 2px solid ${(props) => props.theme.secondaryColor};
+      border-radius: 10px;
+      margin-top: 5%;
+    }
+  }
+  @media (min-width: 600px) {
+    width: 100%;
+    padding: 0 5%;
+    overflow: scroll;
+    ::-webkit-scrollbar {
+      display: none;
+    }
+    ul {
+      list-style: none;
+      padding: 0;
+      color: ${(props) => props.theme.secondaryColor};
+      display: grid;
+      grid-template-columns: 48% 48%;
+      grid-gap: 4%;
+
+      li {
+        width: 100%;
+        background-color: ${(props) => props.theme.primaryColor};
+        border: 2px solid ${(props) => props.theme.secondaryColor};
+        border-radius: 10px;
+        margin-top: 5%;
+      }
+    }
+    @media (min-width: 1200px) {
+    width: 100%;
+    padding: 0 5%;
+    overflow: scroll;
+    ::-webkit-scrollbar {
+      display: none;
+    }
+    ul {
+      list-style: none;
+      padding: 0;
+      color: ${(props) => props.theme.secondaryColor};
+      display: grid;
+      grid-template-columns: 30% 30% 30%;
+      grid-gap: 5%;
+
+      li {
+        width: 100%;
+        background-color: ${(props) => props.theme.primaryColor};
+        border: 2px solid ${(props) => props.theme.secondaryColor};
+        border-radius: 10px;
+        margin-top: 5%;
+      }
     }
   }
 `;
