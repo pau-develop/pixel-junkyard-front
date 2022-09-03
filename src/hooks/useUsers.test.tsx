362896568939bd215a2c9bd1ea1aa7849107a2dd
Pau <103753396+pau-develop@users.noreply.github.com>
2022-09-03 12:56:00 +0200

    Tested getAllUsers function in hook

---
+++ src/hooks/useUsers.test.tsx
@@ -0,0 +1,58 @@
+import { renderHook, waitFor } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import useUsers from "./useUsers";
+
+const mockDispatch = jest.fn();
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+}));
+
+interface WrapperProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+let Wrapper: ({ children }: WrapperProps) => JSX.Element;
+
+beforeEach(() => {
+  Wrapper = ({ children }: WrapperProps): JSX.Element => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+});
+
+describe("Given a useUsers hook", () => {
+  describe("When its function getAllUsers is called", () => {
+    test("It should add all robots from the DB to the store state", async () => {
+      const users = [
+        {
+          userName: "user",
+        },
+        {
+          userName: "user2",
+        },
+      ];
+      global.fetch = jest.fn().mockReturnValue({
+        json: jest.fn().mockReturnValue(users),
+      });
+
+      const {
+        result: {
+          current: { getAllUsers },
+        },
+      } = renderHook(useUsers, { wrapper: Wrapper });
+
+      await waitFor(() => {
+        getAllUsers();
+      });
+
+      const expectedAction = {
+        payload: [{ userName: "user" }, { userName: "user2" }],
+        type: "users@all",
+      };
+
+      expect(mockDispatch).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+});
