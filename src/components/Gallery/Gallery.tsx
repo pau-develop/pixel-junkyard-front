a958c45cc6b9409c55f622f39384544be0384853
Pau <103753396+pau-develop@users.noreply.github.com>
2022-09-05 23:58:19 +0200

    Loaded drawings into Gallery component list

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,7 +1,31 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import useDrawings from "../../hooks/useDrawings";
+import { IDrawing } from "../../interfaces/interfaces";
 import GalleryStyled from "./GalleryStyled";
 
 const Gallery = (): JSX.Element => {
-  return <GalleryStyled></GalleryStyled>;
+  const drawings = useSelector<RootState>(
+    (state) => state.drawings
+  ) as IDrawing[];
+  const { getAllDrawings } = useDrawings();
+
+  useEffect(() => {
+    getAllDrawings();
+  }, [getAllDrawings]);
+
+  console.log(drawings);
+
+  return (
+    <GalleryStyled>
+      <ul>
+        {drawings.map((drawing, index) => (
+          <li key={drawing._id}>{drawing.name}</li>
+        ))}
+      </ul>
+    </GalleryStyled>
+  );
 };
 
 export default Gallery;
