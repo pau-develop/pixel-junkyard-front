0c083337ff00fd00d91891469aeabe9f5c7718cb
Pau <103753396+pau-develop@users.noreply.github.com>
2022-09-06 12:33:31 +0200

    Created & tested DrawingDetailPage component

---
+++ src/pages/DrawingDetailPage/DrawingDetailPage.tsx
@@ -0,0 +1,11 @@
+import PageStyled from "../PageStyled";
+
+const DrawingDetailPage = (): JSX.Element => {
+  return (
+    <PageStyled className="page">
+      <h1 className="page__title">Drawing</h1>
+    </PageStyled>
+  );
+};
+
+export default DrawingDetailPage;
