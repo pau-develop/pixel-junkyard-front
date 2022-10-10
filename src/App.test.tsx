4ce1921030a95b33b9f40c406cb1bc58fcd944a4
Pau <103753396+pau-develop@users.noreply.github.com>
2022-10-11 01:21:43 +0200

    Tested last feature added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { store } from "./app/store";
 import mockStore from "./mocks/mockStore";
+import mockUser from "./mocks/mockUser";
 import * as utils from "./utils/auth";
 
 interface WrapperProps {
@@ -11,6 +12,36 @@ interface WrapperProps {
 }
 
 let Wrapper: ({ children }: WrapperProps) => JSX.Element;
+
+const mockDispatch = jest.fn();
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+}));
+
+const mockGetUserById = jest
+  .fn()
+  .mockReturnValueOnce(mockUser)
+  .mockReturnValueOnce(null);
+
+const mockLogoutUser = jest.fn();
+
+jest.mock("./hooks/useUser", () => ({
+  __esModule: true,
+  ...jest.requireActual("./hooks/useUsers"),
+  default: () => ({
+    logoutUser: () => mockLogoutUser(),
+  }),
+}));
+
+jest.mock("./hooks/useUsers", () => ({
+  __esModule: true,
+  ...jest.requireActual("./hooks/useUsers"),
+  default: () => ({
+    getUserById: () => mockGetUserById(),
+  }),
+}));
 
 beforeEach(() => {
   Wrapper = ({ children }: WrapperProps): JSX.Element => {
@@ -48,10 +79,28 @@ describe("Given an App component", () => {
       expect(modalElement).not.toBeNull();
     });
 
-    test("If there is a token in the storage, fetchToken function should be called", () => {
+    test("If getUserById returns null, the logoutUser hook function should be called", () => {
       const mockToken =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzEwODYxYzk5MGM3MDlhNmNlYjk0NWQiLCJ1c2VyTmFtZSI6InRlc3RpbmciLCJpYXQiOjE2NjIxMzk1Mzd9.EKxxxoIKOLRRPDR4Uuh-_QmFM8khGF4_-mxbIxjrOpE";
       window.localStorage.setItem("token", mockToken);
+      const mockFetch = jest.spyOn(utils, "fetchToken");
+      render(<App />, { wrapper: Wrapper });
+
+      expect(mockFetch).toHaveBeenCalled();
+    });
+
+    test("If there is a token in the storage, fetchToken function should be called, getUserById should be called and finally, loginUser should be called", async () => {
+      const mockToken =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzEwODYxYzk5MGM3MDlhNmNlYjk0NWQiLCJ1c2VyTmFtZSI6InRlc3RpbmciLCJpYXQiOjE2NjIxMzk1Mzd9.EKxxxoIKOLRRPDR4Uuh-_QmFM8khGF4_-mxbIxjrOpE";
+      window.localStorage.setItem("token", mockToken);
+      const mockFetch = jest.spyOn(utils, "fetchToken");
+      render(<App />, { wrapper: Wrapper });
+      expect(mockFetch).toHaveBeenCalled();
+      expect(mockGetUserById).toReturnWith(mockUser);
+    });
+
+    test("If there is no token in the storage, the function should just return", () => {
+      window.localStorage.removeItem("token");
       const mockFetch = jest.spyOn(utils, "fetchToken");
       render(<App />, { wrapper: Wrapper });
 
